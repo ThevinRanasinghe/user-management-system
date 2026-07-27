d5bd792ed9c04062329544ba4be06c922a98b1d1
ThevinRanasinghe <thevinmanvindu@gmail.com>
2026-07-27 22:19:45 +0530

    Add webConfig File

--- src/main/java/com/training/userManagementSystem/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.training.userManagementSystem.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
